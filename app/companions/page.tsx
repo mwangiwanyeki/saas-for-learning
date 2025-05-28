b6d504c83cd95dce8d309a0c949cf60c20ab90db
mwangiwanyeki <37798128+mwangiwanyeki@users.noreply.github.com>
2025-05-28 02:45:37 -0700

    initial commit

---
+++ app/companions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CompnanionLobrary = () => {
+    return (
+        <div>
+            Companion library
+        </div>
+    );
+};
+
+export default CompnanionLobrary;
+
